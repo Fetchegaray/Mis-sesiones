7c434d41bf7cc618e4113470c4c6cf08e8023e2f
Fernando Etchegaray <fjetchegaray@gmail.com>
2026-07-03 22:16:04 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+/* Service worker: cachea la app para que funcione sin internet */
+const CACHE = "mis-sesiones-v3";
+const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+/* Estrategia: red primero (para recibir actualizaciones), caché si no hay conexión */
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request, { ignoreSearch: true }).then((m) => m || caches.match("./index.html")))
+  );
+});
